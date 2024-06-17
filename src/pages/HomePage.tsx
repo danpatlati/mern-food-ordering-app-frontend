27207b3472e43c258e21b71ac41db6e485051102
Dan Patlati <patlatidan1@gmail.com>
2024-06-17 14:58:18 +0300

    added checkout flow

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -19,7 +19,7 @@ const HomePage = () => {
         </h1>
         <span className="text-xl">Food is just a click away!</span>
         <SearchBar
-          placeHolder="Search by City or Town"
+          placeHolder="Search by City or Town. 'London', 'Manchester', 'Tekirova'"
           onSubmit={handleSearchSubmit}
         />
       </div>
